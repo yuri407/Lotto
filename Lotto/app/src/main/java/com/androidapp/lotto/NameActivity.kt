e730ee12a640762fa450a6ab71c9af3f4afac855
yuri <dbfl4387@naver.com>
2021-05-26 14:10:33 +0900

    balls

--- Lotto/app/src/main/java/com/androidapp/lotto/NameActivity.kt
+++ Lotto/app/src/main/java/com/androidapp/lotto/NameActivity.kt
@@ -1,11 +1,22 @@
 package com.androidapp.lotto
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class NameActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_name)
+
+        val btnGo= findViewById<Button>(R.id.btnGo)
+        btnGo.setOnClickListener {
+            startActivity(Intent(this, ResultActivity::class.java))
+        }
+        val btnBack = findViewById<Button>(R.id.btnBack)
+        btnBack.setOnClickListener {
+            finish()
+        }
     }
 }
--- Lotto/app/src/main/java/com/androidapp/lotto/ResultActivity.kt
+++ Lotto/app/src/main/java/com/androidapp/lotto/ResultActivity.kt
@@ -7,5 +7,16 @@ class ResultActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
+
+        val result = intent.getIntegerArrayListExtra("result") // ?: return
+        /*if (result == null) {
+            return
+        }
+        println("result is $result")
+        result.sort()
+        println("result sorted is $result") */
+
+        val result_sorted = result?.sortedBy{it}
+        println("result_sorted is" + result_sorted)
     }
 }
